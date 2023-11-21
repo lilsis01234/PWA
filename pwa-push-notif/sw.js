e99ba68e25c2b8652f3277bc11875cfc5c2cf4a6
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-11-21 09:32:05 +0300

    push notification

---
+++ pwa-push-notif/sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'CT-v1';
+self.addEventListener('install',event=>{
+    event.waitUntil((async()=>{
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll([
+            '/',
+            '/convertir.js',
+            '/convert.css'
+        ]);
+    })());
+})
+self.addEventListener('fetch',event=>{
+    event.respondWith((async()=>{
+        const cache = await caches.open(CACHE_NAME);
+        const cachedResponse = await cache.match(event.request);
+        if(cachedResponse){
+            return cachedResponse;
+        }
+        else{
+            try{
+                const fetchResponse = await fetch(event.request);
+
+                cache.put(event.request,fetchResponse.clone());
+                return fetchResponse;
+            }catch(e){
+                //probleme de reseau
+            }
+        }
+    })());
+});
+self.addEventListener('push',(event)=>{
+    const json = JSON.parse(event.data.text())
+    console.log('push data',event.data.text())
+    self.registration.showNotification(json.header,json.options)
+})
+
